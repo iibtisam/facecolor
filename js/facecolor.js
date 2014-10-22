9e1f0892eeea34837658a509031d26f250c4537b
Ibtisam ul Haq <iibtisam@gmail.com>
2014-10-22 23:51:02 +0500

    add some animation

--- js/facecolor.js
+++ js/facecolor.js
@@ -1,7 +1,11 @@
+winHeight = 0;
+winWidth = 0;
 
 $(document).ready(function(){
 	homeCols();
 	adjustHomeColumnHeight();
+	menuEffect();
+
 });
 
 
@@ -11,27 +15,52 @@ $( window ).resize(function() {
 });
 
 
-
+var menuEffect = function(){
+	$(".main-menu div").hover(function(){ //mouseenter
+       	$(this).animate({"top":-10, "height": 41, "padding-top":23},200);
+    }, function(){ //mouseleave
+       	$(this).animate({"top":-25, "height": 33, "padding-top":31},50);
+    });
+}
 
 var homeCols = function(){
 
-	var winHeight = $(window).height() - 65;
-	var winWidth = $(window).width();
+	winHeight = $(window).height() - 65;
+	winWidth = $(window).width();
 
-	$('.parallax-bg').css('height',winHeight);
-	$('.home-parallax').css('height',winHeight);
+	// $('.parallax-bg').css('height',winHeight);
+	// $('.home-parallax').css('height',winHeight);
 	$('.homeColumn').css('height',winHeight);
 	$('.homeColumn').css('width',(winWidth/3)-1);
-
 
 }
 
 var adjustHomeColumnHeight = function(){
+	
 	var winHeight = $('.homeColumn').height();
-	for(var i=0; i< $('.overlayHomeColumn').length; i++){
-		var textHeight = $($('.overlayHomeColumn')[i]).siblings('.overlayHomeColumnText').height();
-		$($('.overlayHomeColumn')[i]).css('height',winHeight-(textHeight+50));
+
+	
+	for(var i=0; i< $('.homeColumn').length; i++){
+		var textHeight = $($(".homeColumn")[i]).children('.overlayHomeColumnText').height()+50;
+		var overlayHeight = winHeight - textHeight;
+		
+		$($('.overlayHomeColumn')[i]).css('height',overlayHeight);
+	
+		$($(".homeColumn")[i]).unbind("hover")
+		$($(".homeColumn")[i]).hover(function(){ //mouseenter
+	
+	       	$(this).children('.overlayHomeColumn').animate({height:0},200);
+	       	$(this).children('.overlayHomeColumnText').delay(200).animate({'bottom': overlayHeight/2},200);
+	
+	    }, function(){ //mouseleave
+	
+	        $(this).children('.overlayHomeColumn').delay(200).animate({height:overlayHeight},200);
+	        $(this).children('.overlayHomeColumnText').animate({'bottom': 0},200);
+	
+	    });
 	}
+
+	$($('#joinus').children('.parallax-bg')).css("height","100%");
 }
 
 
@@ -47,14 +76,12 @@ var openColumnInfo = function(col_no){
 
 var removeOverlay = function(that){
 
-console.log("removeOverlay");
 	var center = ($('.homeColumn').height()/2) - ($(that).children('.overlayHomeColumnText').height()+50)/2;
 
-	$(that).children('.overlayHomeColumn').hide();
 	$(that).children('.overlayHomeColumnText').css('bottom', center);
+
 }
 
 var resetOverlay = function(that){
-	$(that).children('.overlayHomeColumn').show();
 	$(that).children('.overlayHomeColumnText').css('bottom', 0);
 }
